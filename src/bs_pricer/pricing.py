eeab6de51751a9ed317d9c9bc68352fa231aa1bb
anderson930420 <bk20040420@gmaIl.com>
2025-12-21 06:58:04 +0800

    Add pricing module skeleton

---
+++ src/bs_pricer/pricing.py
@@ -0,0 +1,27 @@
+'''Only Pricing function for Black-Scholes-Merton model
+    No validation / IO / formatting'''
+
+def _d1_d2(S, K, sigma, T, r):
+        raise NotImplementedError
+
+def _norm_cdf(x):
+        raise NotImplementedError
+
+def price(S, K, sigma, T, r):
+    '''
+    Docstring for price
+    :param S: Price of the underlying asset at time t
+    :param K: Strike price of the option
+    :param sigma: Volatility (annualized standard deviation of returns)
+    :param T: Time of option expiration
+    :param r: Annualized risk-free interest rate
+    Outputs call and put prices in dict format, e.g. {'call': call_price, 'put': put_price} 
+    '''
+    #Receive inputs: S, K, sigma, T, r
+    #Compute shared intermediate values: vol_sqrt_t
+    #Compute d1 and d2 -> (call _d1_d2)
+    #Evaluate N(d1) and N(d2) -> (call _norm_cdf) twice
+    #Assemble the call price
+    #Assemble the put price
+    #Return the results
+    raise NotImplementedError
